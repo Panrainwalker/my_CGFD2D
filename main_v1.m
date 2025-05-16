7ac6c8f6a15222d0e263fb21130edf72a8a7f8d8
Yuxing Pan <panyx2023@mail.sustech.edu.cn>
2025-05-16 23:51:07 +0800

    Initial commit

---
+++ main_v1.m
@@ -0,0 +1,11 @@
+% -------------------------------------------------------------------------
+% 2D Elastic Wave Simulation in an Isotropic Medium
+% This script simulates elastic wave propagation using a collocated-grid 
+% finite-difference method in a 2D isotropic medium. 
+% Note: Curvilinear grid implementation is not included in this version.
+%
+% Author: Yuxing Pan (panyx2023@mail.sustech.edu.cn)
+% Affiliation: Southern University of Science and Technology (SUSTech)
+% Date: May 16, 2025
+% -------------------------------------------------------------------------
+
